cfeae8b74b50ec55196bee21f90f50bdb43ea03a
aasmaanchaalak <84654827+aasmaanchaalak@users.noreply.github.com>
2021-11-21 16:30:12 +0530

    organised leads into a list

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    renderLeads();
+    inputEl.value = "";
 })
+
+function renderLeads(){
+    for (let i=0; i<myLeads.length; i++){
+        ulEl.innerHTML += `<li><a href='https://${myLeads[i]}' target='_blank'>${myLeads[i]}</a></li>`;
+    }
+}
