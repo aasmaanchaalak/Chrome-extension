6aabac46047bdd0c679e12ebe152b43cf7447087
aasmaanchaalak <84654827+aasmaanchaalak@users.noreply.github.com>
2021-11-21 16:07:26 +0530

    setup

---
+++ index.js
@@ -0,0 +1,9 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+})
