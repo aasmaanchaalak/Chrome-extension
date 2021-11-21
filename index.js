cd937acdd74234659b8b60d3ba6738e462cb5fab
aasmaanchaalak <84654827+aasmaanchaalak@users.noreply.github.com>
2021-11-21 17:15:08 +0530

    added localStorage

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage;
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
     renderLeads();
@@ -10,7 +16,10 @@ inputBtn.addEventListener("click", function(){
 })
 
 function renderLeads(){
+    let listItems = "";
     for (let i=0; i<myLeads.length; i++){
-        ulEl.innerHTML += `<li><a href='https://${myLeads[i]}' target='_blank'>${myLeads[i]}</a></li>`;
+        listItems += `<li><a href='https://${myLeads[i]}' target='_blank'>${myLeads[i]}</a></li>`;
     }
+    ulEl.innerHTML = listItems;
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
 }
